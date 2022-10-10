2a40e57d26cf8f0cec0cfa11fd59a65000a5cc91
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2022-10-10 11:50:20 +0530

    minor changes

--- src/assignment1/Question4.java
+++ src/assignment1/Question4.java
@@ -3,6 +3,18 @@ package assignment1;
 
 public class Question4 {
     public static void main(String[] args) {
+        boolean a, b, c , temp, sum;
+        a = true;
+        b = true;
+        c = false;
 
+        sum = a ^ b;
+        temp = a & b;
+
+        System.out.println("Value of a is : " + a);
+        System.out.println("Value of b is : " + b);
+        System.out.println("Value of c is : " + c);
+        System.out.println("Value of sum is : " + sum);
+        System.out.println("Value of temp is : " + temp);
     }
 }
