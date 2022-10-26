a72a1bb03072e583b0591d1cc190261f8da7bcb1
Manas Ranjan Mohanta <manasranjanmohanta679@gmail.com>
2022-10-26 23:04:22 +0530

    Did Question-6

---
+++ src/assignment2/Question6.java
@@ -0,0 +1,32 @@
+//6. Write a java program to check weather the given number is palindrome and prime or not?
+package assignment2;
+
+import java.util.Scanner;
+
+public class Question6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        int n1 = n, rev = 0, count = 0;
+
+        while(n > 0){
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n = n / 10;
+        }
+
+        for(int i = 1; i <= n1; i++){
+            if(n1 % i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println(n1 + " is a prime number");
+        }
+        if(rev == n1){
+            System.out.println(n1 + " is a palindrome number");
+        }
+
+    }
+}
